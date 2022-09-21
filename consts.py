e2859cf50b00f717ec389b1a88d9a83382165416
yarinsudai66 <yarinsudai66@gmail.com>
2022-09-21 13:59:13 +0300

    add fghj

--- consts.py
+++ consts.py
@@ -1,5 +1,35 @@
-import os
 import pygame
+import os
+
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 500
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+STEP = 20
+GARBAGE_LAND_PIC ="pass"
+GARBAGE_SEA_PIC ='DF'
+GARBAGE_AIR_PIC = 'DFB'
+SCREEN_SEA_PIC = "pass"
+SCREEN_LAND_PIC = "PASS"
+SCREEN_AIR_PIC ='PASS'
+
+pygame.font.init()
+FONT_NAME = "Ariel"
+LOSE_MESSAGE = "You Lost!"
+LOSE_FONT_SIZE = int(0.15 * WINDOW_WIDTH)
+LOSE_COLOR = (0, 0, 0)
+LOSE_LOCATION = \
+    (0.2 * WINDOW_WIDTH, WINDOW_HEIGHT / 2 - (LOSE_FONT_SIZE / 2))
+WIN_MESSAGE = "You Won! " \
+              "Thank you for cleaning up the world"
+WIN_FONT_SIZE = LOSE_FONT_SIZE
+WIN_COLOR = (89, 89, 89)
+WIN_LOCATION = \
+    (0.2 * WINDOW_WIDTH, WINDOW_HEIGHT / 2 - (WIN_FONT_SIZE / 2))
+FIRST_MESSAGE = " Welcome to the cleaning game! " \
+                "Let's clean up our world and keep it clean and happy" \
+                "Have Fun!"
+FIRST_FONT_SIZE = int(15)
+FIRST_LOCATION = (20, 20)
 
 SCREEN_ROWS = 10
 SCREEN_COLS = 10
@@ -12,5 +42,5 @@ GARBAGE_COUNT = 5
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 500
 FPS = 60
-GARBAGE_MAN_PIC = pygame.image.load(os.path.join('pictures', 'garbage_man.png'))
-SCREEN_PIC = pygame.image.load(os.path.join('pictures', 'screen.png'))
+# GARBAGE_MAN_PIC = pygame.image.load(os.path.join('pictures', 'garbage_man.png'))
+SCREEN_PIC = pygame.image.load(os.path.join('pics', 'screen.png'))
---
+++ screen.py
@@ -0,0 +1,49 @@
+import pygame
+import consts
+import random
+
+# coloring window
+def draw_window(pic):
+    consts.WINDOW.fill(pic, (0, 0))
+    pygame.display.update()
+
+def draw_garbage(garbages):
+    for gar in range(0, len(garbages)):
+        consts.WINDOW.blit(consts.GARBAGE_PIC, (garbages[gar][0] * consts.STEP, garbages[gar][1] * consts.STEP))
+
+def draw_screen():
+    draw_window()
+    # draw_garbage(garbages= list)
+
+def draw_message(message, font_size, color, location):
+    font = pygame.font.SysFont(consts.FONT_NAME, font_size)
+    text_img = font.render(message, True, color)
+    consts.WINDOW.blit(text_img, location)
+
+
+def draw_lose_message():
+    draw_message(consts.LOSE_MESSAGE, consts.LOSE_FONT_SIZE, consts.WIN_COLOR, consts.LOSE_LOCATION)
+    pygame.display.update()
+
+
+def draw_win_message():
+    draw_message(consts.WIN_MESSAGE, consts.WIN_FONT_SIZE, consts.WIN_COLOR, consts.WIN_LOCATION)
+    pygame.display.update()
+
+
+def draw_first_message():
+    draw_message(consts.FIRST_MESSAGE, consts.FIRST_FONT_SIZE, (255, 255, 255), consts.FIRST_LOCATION)
+    pygame.display.update()
+
+def play():
+    draw_window(consts.SCREEN_PIC, (0, 0, 0))
+    # draw_screen()
+    draw_first_message()
+
+    finish = False
+    while not finish:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finish = True
+
+
